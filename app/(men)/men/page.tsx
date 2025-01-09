9fd5bc7a2f68363c480293bef1cfccabc14705b4
Zakaria Asad <zasad2587@gmail.com>
2025-01-09 15:48:26 +0200

    fetch the men underwire from the database and show it

--- app/(men)/men/page.tsx
+++ app/(men)/men/page.tsx
@@ -1,15 +1,22 @@
 import ContentSection from "@/components/men-page/ContentSection";
 import SideSection from "@/components/men-page/SideSection";
 import { getAlAseelClothes } from "@/queries/getAlAseelClothes";
+import { getMenUnderWire } from "@/queries/getMenUnderWire";
 
 const MenPage = async () => {
   const menClothes = await getAlAseelClothes();
+  const menUnderWire = await getMenUnderWire();
 
   return (
     <div className="flex flex-col">
       <div className="grid grid-cols-[200px_1fr] h-auto mt-20">
         <SideSection heading="Alaseel Clothes" href="/" linkLabel="Show All" />
         <ContentSection data={menClothes} />
+      </div>
+
+      <div className="grid grid-cols-[200px_1fr] h-auto mt-20">
+        <SideSection heading="Underwire" href="/" linkLabel="Show All" />
+        <ContentSection data={menUnderWire} isVertical />
       </div>
     </div>
   );
