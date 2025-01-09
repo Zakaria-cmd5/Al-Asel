0d23e2157819db22b3fe1f99b3c834512122c48f
Zakaria Asad <zasad2587@gmail.com>
2025-01-09 21:11:19 +0200

    modify the width of the youth page image

--- app/(youth)/youth/page.tsx
+++ app/(youth)/youth/page.tsx
@@ -13,7 +13,7 @@ const YouthPage = async () => {
 
   return (
     <div className="mt-10 flex flex-col gap-10">
-      <Image src={youthImage} alt="Youth Image" className="w-[50%] mx-auto" />
+      <Image src={youthImage} alt="Youth Image" className="w-[75%] mx-auto" />
 
       <div className="grid grid-cols-[200px_1fr] h-auto mt-20">
         <SideSection heading="Youth Dress" href="/" linkLabel="Show All" />
