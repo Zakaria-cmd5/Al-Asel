72d9812afb7209d719f81bd372c6cf3854802df2
Zakaria Asad <zasad2587@gmail.com>
2025-01-12 19:54:44 +0200

    display form error

--- actions/addDressAction.ts
+++ actions/addDressAction.ts
@@ -12,6 +12,11 @@ interface NewDressFormState {
     price?: string[];
     code?: string[];
     image?: string[];
+    colors?: string[];
+    sizes?: string[];
+    dressLength?: string[];
+    mainCategory?: string[];
+    subCategory?: string[];
   };
   message?: string | null;
 }
@@ -20,17 +25,17 @@ const newDressSchema = z.object({
   name: z
     .string()
     .min(3, "Product name is too short")
-    .max(100, "Product name is too long") // Increased max length
+    .max(100, "Product name is too long")
     .trim(),
   description: z
     .string()
     .min(3, "Description is too short")
-    .max(1000, "Description is too long") // Increased max length
+    .max(1000, "Description is too long")
     .trim(),
   code: z
     .string()
     .min(3, "Product code is too short")
-    .max(50, "Product code is too long") // Increased max length
+    .max(50, "Product code is too long")
     .trim(),
   price: z
     .string()
@@ -38,33 +43,29 @@ const newDressSchema = z.object({
     .refine((val) => !isNaN(val), "Price must be a valid number"),
 });
 
-export async function addDressAction(
-  formData: FormData,
-  image: string,
-  colors: string[],
-  sizes: string[],
-  dressLength: string[],
-  mainCategory: string,
-  subCategory: string
-): Promise<NewDressFormState> {
+export async function addDressAction(payload: {
+  formData: FormData;
+  image: string;
+  colors: string[];
+  sizes: string[];
+  dressLengths: string[];
+  mainCategory: string;
+  subCategory: string;
+}): Promise<NewDressFormState> {
+  const {
+    formData,
+    image,
+    colors,
+    sizes,
+    dressLengths,
+    mainCategory,
+    subCategory,
+  } = payload;
+
   const name = formData.get("name") as string;
   const description = formData.get("description") as string;
   const code = formData.get("code") as string;
   const price = formData.get("price") as string;
-
-  console.log("Form Data:");
-  console.log({
-    name,
-    description,
-    code,
-    price,
-    image,
-    colors,
-    sizes,
-    dressLength,
-    mainCategory,
-    subCategory,
-  });
 
   const validation = newDressSchema.safeParse({
     name,
@@ -73,20 +74,44 @@ export async function addDressAction(
     price,
   });
 
+  const errors: NewDressFormState["errors"] = {};
+
   if (!validation.success) {
-    console.error("Validation Errors:", validation.error.flatten().fieldErrors);
+    Object.assign(errors, validation.error.flatten().fieldErrors);
     return {
-      errors: validation.error.flatten().fieldErrors,
-      message: "Please enter valid fields",
+      errors,
+      message: "Please correct the highlighted errors.",
     };
   }
 
   if (!image) {
+    errors.image = ["Please upload an image."];
+  }
+
+  if (colors.length === 0) {
+    errors.colors = ["Please add at least one color."];
+  }
+
+  if (sizes.length === 0) {
+    errors.sizes = ["Please add at least one size."];
+  }
+
+  if (dressLengths.length === 0) {
+    errors.dressLength = ["Please add at least one dress length."];
+  }
+
+  if (!mainCategory) {
+    errors.mainCategory = ["Please select a main category."];
+  }
+
+  if (!subCategory) {
+    errors.subCategory = ["Please select a sub-category."];
+  }
+
+  if (Object.keys(errors).length > 0) {
     return {
-      errors: {
-        image: ["Please upload an image before submitting."],
-      },
-      message: "Validation failed",
+      errors,
+      message: "Please correct the highlighted errors.",
     };
   }
 
@@ -98,7 +123,7 @@ export async function addDressAction(
       dressCode: validation.data.code,
       image,
       dressLength: {
-        create: dressLength.map((length) => ({ name: length })),
+        create: dressLengths.map((length) => ({ name: length })),
       },
       mainCategory: mainCategory.toUpperCase() as any,
       subCategory: subCategory.toUpperCase() as any,
--- app/(new-dress)/new-dress/page.tsx
+++ app/(new-dress)/new-dress/page.tsx
@@ -1,43 +1,45 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// @ts-nocheck
 "use client";
 
 import { addDressAction } from "@/actions/addDressAction";
 import FormSelect from "@/components/custom-ui/FormSelect";
 import MultiValueInput from "@/components/new-dress-page/MultiValueInput";
 import { CldUploadWidget } from "next-cloudinary";
-import { useState } from "react";
+import { useActionState, useState } from "react";
 
 const NewDressPage = () => {
   const [dressLengths, setDressLengths] = useState<string[]>([]);
   const [sizes, setSizes] = useState<string[]>([]);
   const [colors, setColors] = useState<string[]>([]);
-  const [image, setImage] = useState<any>("");
+  const [image, setImage] = useState<any>(null);
   const [mainCategory, setMainCategory] = useState<string>("");
   const [subCategory, setSubCategory] = useState<string>("");
 
-  const mainCategoryValueChange = (value: string) => {
-    setMainCategory(value);
-  };
+  const initState = { errors: {}, message: "" };
 
-  const subCategoryValueChange = (value: string) => {
-    setSubCategory(value);
+  const [formError, dispatch] = useActionState(
+    async (_: any, payload: Parameters<typeof addDressAction>[0]) =>
+      await addDressAction(payload),
+    initState
+  );
+
+  const handleSubmit = (formData: FormData) => {
+    const payload = {
+      formData,
+      image: image?.secure_url || "",
+      colors,
+      sizes,
+      dressLengths,
+      mainCategory,
+      subCategory,
+    };
+
+    dispatch(payload);
   };
 
   return (
     <form
-      action={(formData) =>
-        addDressAction(
-          formData,
-          image.secure_url,
-          colors,
-          sizes,
-          dressLengths,
-          mainCategory,
-          subCategory
-        )
-      }
+      action={handleSubmit}
       className="flex flex-col items-center mt-10 mb-2 p-6 bg-white rounded-lg shadow-lg w-full max-w-md mx-auto"
     >
       <h2 className="text-2xl font-bold text-amber-600 mb-6">Add New Item</h2>
@@ -50,8 +52,30 @@ const NewDressPage = () => {
           type="text"
           name="name"
           placeholder="Enter dress name"
-          className="w-full px-4 py-2 border border-amber-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-400 focus:border-amber-400"
+          className="w-full px-4 py-2 border border-amber-300 rounded-lg shadow-sm focus:outline-none"
         />
+        {formError.errors?.name && (
+          <p className="text-red-500 text-sm mt-1">
+            {formError.errors.name[0]}
+          </p>
+        )}
+      </div>
+
+      <div className="w-full mb-4">
+        <label htmlFor="code" className="block text-amber-700 font-medium mb-1">
+          Code
+        </label>
+        <input
+          type="text"
+          name="code"
+          placeholder="Enter dress code"
+          className="w-full px-4 py-2 border border-amber-300 rounded-lg shadow-sm focus:outline-none"
+        />
+        {formError.errors?.code && (
+          <p className="text-red-500 text-sm mt-1">
+            {formError.errors.code[0]}
+          </p>
+        )}
       </div>
 
       <div className="w-full mb-4">
@@ -65,75 +89,77 @@ const NewDressPage = () => {
           name="description"
           placeholder="Enter description"
           rows={4}
-          className="w-full px-4 py-2 border border-amber-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-400 focus:border-amber-400"
+          className="w-full px-4 py-2 border border-amber-300 rounded-lg shadow-sm focus:outline-none"
         />
+        {formError.errors?.description && (
+          <p className="text-red-500 text-sm mt-1">
+            {formError.errors.description[0]}
+          </p>
+        )}
       </div>
 
-      <div className="w-full mb-4">
-        <label htmlFor="name" className="block text-amber-700 font-medium mb-1">
-          Product Code
-        </label>
-        <input
-          type="text"
-          name="code"
-          placeholder="Enter dress name"
-          className="w-full px-4 py-2 border border-amber-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-400 focus:border-amber-400"
-        />
-      </div>
+      <MultiValueInput
+        label="Colors"
+        placeholder="Enter color"
+        onValuesChange={setColors}
+      />
+      {formError.errors?.colors && (
+        <p className="text-red-500 text-sm mt-1">
+          {formError.errors.colors[0]}
+        </p>
+      )}
 
-      <div className="w-full mb-4">
-        <label
-          htmlFor="price"
-          className="block text-amber-700 font-medium mb-1"
-        >
-          Price
-        </label>
-        <input
-          type="number"
-          name="price"
-          placeholder="Enter price"
-          className="w-full px-4 py-2 border border-amber-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-400 focus:border-amber-400"
-        />
-      </div>
+      <MultiValueInput
+        label="Sizes"
+        placeholder="Enter size"
+        onValuesChange={setSizes}
+      />
+      {formError.errors?.sizes && (
+        <p className="text-red-500 text-sm mt-1">{formError.errors.sizes[0]}</p>
+      )}
 
       <MultiValueInput
         label="Dress Lengths"
         placeholder="Enter dress length"
         onValuesChange={setDressLengths}
       />
-
-      <MultiValueInput
-        label="Sizes"
-        placeholder="Enter size (e.g., S, M, L)"
-        onValuesChange={setSizes}
-      />
-
-      <MultiValueInput
-        label="Colors"
-        placeholder="Enter color (e.g., Red, Blue)"
-        onValuesChange={setColors}
-      />
+      {formError.errors?.dressLength && (
+        <p className="text-red-500 text-sm mt-1">
+          {formError.errors.dressLength[0]}
+        </p>
+      )}
 
       <FormSelect
-        mainCategoryValueChange={mainCategoryValueChange}
-        subCategoryValueChange={subCategoryValueChange}
+        mainCategoryValueChange={setMainCategory}
+        subCategoryValueChange={setSubCategory}
       />
+      {formError.errors?.mainCategory && (
+        <p className="text-red-500 text-sm mt-1">
+          {formError.errors.mainCategory[0]}
+        </p>
+      )}
+      {formError.errors?.subCategory && (
+        <p className="text-red-500 text-sm mt-1">
+          {formError.errors.subCategory[0]}
+        </p>
+      )}
 
       <CldUploadWidget
         uploadPreset="al-aseel"
         onSuccess={(result) => setImage(result.info)}
       >
-        {({ open }) => {
-          return (
-            <div
-              className="cursor-pointer mt-4 px-6 py-2 bg-amber-500 text-white font-bold rounded-lg hover:bg-amber-600 transition-all shadow-lg"
-              onClick={() => open()}
-            >
-              Upload an Image
-            </div>
-          );
-        }}
+        {({ open }) => (
+          <div
+            className="cursor-pointer mt-4 px-6 py-2 bg-amber-500 text-white font-bold rounded-lg hover:bg-amber-600 transition-all shadow-lg"
+            onClick={() => open()}
+          >
+            Upload an Image
+          </div>
+        )}
       </CldUploadWidget>
+      {formError.errors?.image && (
+        <p className="text-red-500 text-sm mt-1">{formError.errors.image[0]}</p>
+      )}
 
       <button
         type="submit"
@@ -141,6 +167,9 @@ const NewDressPage = () => {
       >
         Add Dress
       </button>
+      {formError.message && (
+        <p className="text-red-500 text-sm mt-4">{formError.message}</p>
+      )}
     </form>
   );
 };
