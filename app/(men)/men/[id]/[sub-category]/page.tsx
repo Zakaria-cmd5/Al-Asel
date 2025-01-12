b025ea5d90acac2907d5a841f60c49181bfd143c
Zakaria Asad <zasad2587@gmail.com>
2025-01-12 21:07:22 +0200

    build the first layout of the men detail page

--- app/(men)/men/[id]/[sub-category]/page.tsx
+++ app/(men)/men/[id]/[sub-category]/page.tsx
@@ -21,20 +21,64 @@ const MenDetailPage = async (props: { params: Params }) => {
   if (!dress) return <p>no dress found</p>;
 
   return (
-    <div>
-      <p>{dress?.name}</p>
-      <p>{dress?.description}</p>
-      <p>{dress?.dressCode}</p>
-      <Image src={dress?.image} alt={dress?.name} width={100} height={100} />
-      {dress.colors.map((c) => (
-        <p key={c.id}>{c.name}</p>
-      ))}
-      {dress.sizes.map((s) => (
-        <p key={s.id}>{s.name}</p>
-      ))}
-      {dress.dressLength.map((d) => (
-        <p key={d.id}>{d.name}</p>
-      ))}
+    <div className="grid grid-cols-2 gap-5 m-5">
+      <div className="mt-12">
+        <Image src={dress.image} alt={dress.name} width={500} height={500} />
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-4">
+          <p className="text-2xl font-bold text-black">{dress.name}</p>
+          <p className="text-sm text-gray-400">{dress.dressCode}</p>
+        </div>
+
+        <div className="flex gap-10 items-center">
+          <p className="text-[#a07653] text-4xl">{dress.price} ر.س</p>
+          <p className="text-2xl">The price is inclusive of tax</p>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-lg font-bold text-black">Size</p>
+          <div className="flex gap-3">
+            {dress.sizes.map((s) => (
+              <div
+                key={s.id}
+                className="flex items-center justify-center border p-3 bg-gray-400 hover:bg-[#a07653] cursor-pointer max-h-[35px] max-w-[55px]"
+              >
+                <p className="text-white">{s.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-lg font-bold text-black">Color</p>
+          <div className="flex gap-3">
+            {dress.colors.map((c) => (
+              <div
+                key={c.id}
+                className="flex items-center justify-center border p-3 bg-gray-400 hover:bg-[#a07653] cursor-pointer max-h-[35px] max-w-[55px]"
+              >
+                <p className="text-white">{c.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-lg font-bold text-black">Thoub length</p>
+          <div className="flex gap-3">
+            {dress.dressLength.map((d) => (
+              <div
+                key={d.id}
+                className="flex items-center justify-center border p-3 bg-gray-400 hover:bg-[#a07653] cursor-pointer max-h-[35px] max-w-[55px]"
+              >
+                <p className="text-white">{d.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
